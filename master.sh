422d88ab71080de7fc21ab75a4bc347c860f5bcd
tjosenascimento1986 <tjosenascimento@gmail.com>
2025-01-10 12:44:52 -0300

    subindo projeto

--- master.sh
+++ master.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=10.10.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+sudo docker node update --availability drain master
+sudo docker service create --name web-server --replicas 15 -p 80:80 httpd
